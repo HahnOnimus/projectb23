9ebf9d53b567c3ad1c21393cbe542c1f1e71e4f1
Hahn Onimus <69061493+HahnOnimus@users.noreply.github.com>
2025-09-21 12:57:58 +0100

    new mods

--- script-one.js
+++ script-one.js
@@ -399,8 +399,8 @@ function animateValue(element, start, end, duration, formatter = null) {
 
 // Countdown Timer
 function initCountdown() {
-    // Target date: September 30, 2025
-    const targetDate = new Date('October 15, 2025 00:00:00').getTime();
+    // Target date: October 30, 2025
+    const targetDate = new Date('October 30, 2025 00:00:00').getTime();
     
     // Get countdown elements
     const daysElement = document.getElementById('countdown-days');
